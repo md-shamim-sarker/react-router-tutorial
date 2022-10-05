de526620012c780ba3ff379ace881fab34867c75
shamimspro <shamimspro@gmail.com>
2022-10-05 21:59:35 +0600

    nested route

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import ErrorPage from './error-page';
 import './index.css';
+import Contact from './routes/contact';
+import Root from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact></Contact>
+      },
+    ]
   },
 ]);
 
